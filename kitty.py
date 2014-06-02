7ea577eaec648d4dcf3f6a6334bf438ba2d8c00b
Louis Taylor <kragniz@gmail.com>
2014-06-02 00:12:42 +0000

    Move arduino into main driver

--- kitty.py
+++ kitty.py
@@ -3,6 +3,10 @@ from threading import Lock
 import json
 import serial
 
+import boatd
+assert boatd.VERSION == 1.1
+
+
 class Arduino(object):
     '''The arduino and basic communications with devices attached to it'''
     def __init__(self, port=None):
@@ -40,9 +44,11 @@ class Arduino(object):
         '''Set the sail to an amount between 1000 and 2000'''
         return self.send_command('s{}'.format(amount)).get('sail')
 
-if __name__ == '__main__':
-    import time
-    a = Arduino('/dev/arduino')
-    print a.get_compass()
-    print a.set_rudder(2000)
-    print a.set_sail(1500)
+
+driver = boatd.Driver()
+
+arduino = Arduino('/dev/arduino')
+
+@driver.heading
+def kitty_heading():
+    return arduino.get_compass()
